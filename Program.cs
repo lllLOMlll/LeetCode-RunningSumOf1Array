8a6b32843c9932c966218ba35313d75b0d7a0514
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-11-01 07:20:52 -0400

    First and last commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+ public class Solution {
+    public int[] RunningSum(int[] nums) {
+        int sum = 0;
+        int[] sumArray = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            sumArray[i] += sum;
+           
+        }
+
+        return sumArray;
+    }
+}
+
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = [1, 2, 3, 4];
+        int[] nums2 = [2, 2 ,2 , 2];
+        int[] result = solution.RunningSum(nums);
+        int[] result2 = solution.RunningSum(nums2);
+        // Print the result array
+        Console.WriteLine(string.Join(", ", result));  
+        Console.WriteLine(string.Join(", ", result2));
+       
+        
+    }
+}
